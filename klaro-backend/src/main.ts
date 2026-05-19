227f543bf6ed9be8a7ce08c11ef9de91a4d03458
Faiza RABHI <93930498+Faizarabhi@users.noreply.github.com>
2026-05-19 12:32:09 +0100

    feat(backend): add swagger documentation and configuration fixes

--- klaro-backend/src/main.ts
+++ klaro-backend/src/main.ts
@@ -1,10 +1,23 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+async function bootstrap(): Promise<void> {
+  const app: INestApplication = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
+
+  //configuration Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Klaro API')
+    .setDescription("API documentation pour le module Demandes d'aide")
+    .setVersion('1.0')
+    .addServer('/api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api-docs', app, document);
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -16,4 +29,7 @@ async function bootstrap() {
   app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
-bootstrap();
+
+bootstrap().catch((err) => {
+  console.error('Erreur lors du démarrage du serveur:', err);
+});
